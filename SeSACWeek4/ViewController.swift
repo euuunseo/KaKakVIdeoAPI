76cc6c43c460eec531b10beb10e97e78eb62036c
euuunseo <gjgh1004@gmail.com>
2023-10-18 00:50:36 +0900

    Initial Commit

---
+++ SeSACWeek4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek4
+//
+//  Created by 고은서 on 2023/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
